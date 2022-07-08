c6d8286e1d88cdb7ad82f55e62b2cbcd17baaf68
Hamza Shiwani <hamza.shiwani@inoviotech.com>
2022-07-08 22:44:29 +0500

    forget api

--- resources/views/emails/myDemoMail.blade.php
+++ resources/views/emails/myDemoMail.blade.php
@@ -6,6 +6,11 @@
 </head>
 <body>
 
+ <h1>{{$details['mail_title']}}</h1>
+ <p>Full Name: {{$details['f_name'] }}  {{$details['l_name'] }}</p>
+ <p>Email:  {{$details['email'] }} </p>
+ <p>Mobile Number: {{$details['phone'] }}</p>
+
  <p>Thank you</p>
 
 </body>
