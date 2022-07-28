33ebae6821e24d0781c692bb5e6659dc9fde8a37
Mujtab Ahmed <mujtaba@inoviotech.com>
2022-07-28 14:28:40 +0500

    some changes.

--- app/Model/ShippingAddress.php
+++ app/Model/ShippingAddress.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShippingAddress extends Model
 {
+    protected $guarded = [];
+
     protected $casts = [
         'customer_id' => 'integer',
     ];
